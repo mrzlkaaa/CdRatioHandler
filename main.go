3eed4ef88209ff7186b397bc71d678e7f9c5171f
Nikita_Ubuntu <nvs38@tpu.ru>
2021-10-30 22:57:34 +0700

    add funcs to fulfill array from imp.txt and also computate ResI

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"strconv"
+	// "log"
 )
 
 type DataSet struct {
@@ -15,13 +17,15 @@ type DataSet struct {
 	sig []float64
 	thermalSig float64
 	epicadmiumSig []float64
+	resonseI []float64
+	sumI float64
 }
 var thermalEn float64 = 0.0253
 var epicadmiumEn float64 = 0.4
 
 func main() {
-	handlingFile("inp.txt")
-	// fmt.Println(data)
+	array := handlingFile("inp.txt")
+	fmt.Println(array)
 	url := "https://www-nds.iaea.org/exfor/servlet/E4sGetTabSect?SectID=9022734&req=2566&PenSectID=13660487&json"
 	bytesBody := request(&url)
 	var results map[string]interface{}
@@ -31,31 +35,54 @@ func main() {
 	fmt.Printf("%T\n", pts)
 	ds := &DataSet{}
 	fulfilingStruct(pts, ds)
-	fmt.Println(ds.thermalSig)
 }
 
-func handlingFile(name string) {
+func convertStrings(array []string) []float64{
+	floated := []float64{}
+	for _, v := range array {
+		floatv, _ := strconv.ParseFloat(v, 64)
+		floated = append(floated, floatv)
+	}
+	return floated
+}
+
+func handlingFile(name string) [][]float64{
 	file, _ := os.Open(name)
 	scan := bufio.NewScanner(file)
 	var linesArray [][]float64
-	var incr int = 0
 	for scan.Scan(){
-		linesArray[] := strings.Split(scan.Text(), "  ")
-		fmt.Println(splitting)
-		incr++
+		if datArray := strings.Split(scan.Text(), "   "); len(datArray) > 1 {
+			floatArray := convertStrings(datArray)
+			// fmt.Println(floatArray)
+			linesArray = append(linesArray, floatArray)
+		}
 	}
+	return linesArray
 }
 
 func fulfilingStruct(intr []interface{}, DS *DataSet) {
-	// intrPrefix := intr.(map[string]interface{})
 	for _, v := range intr {
-		// fmt.Println(v.(map[string]interface{})["E"])
 		DS.energy = append(DS.energy, v.(map[string]interface{})["E"].(float64))
 		DS.sig = append(DS.sig, v.(map[string]interface{})["Sig"].(float64))
 		if v.(map[string]interface{})["E"].(float64) == thermalEn {
 			DS.thermalSig = v.(map[string]interface{})["Sig"].(float64)
 		} else if v.(map[string]interface{})["E"].(float64) > epicadmiumEn {
 			DS.epicadmiumSig = append(DS.epicadmiumSig, v.(map[string]interface{})["E"].(float64))
+		}
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+			fmt.Println("Finished looping in sigms")
+		}
+	}()
+	for i:=0; i <= len(DS.sig); i++ {
+		if DS.energy[i] > 0.5 {
+			DS.resonseI = append(DS.resonseI, DS.sig[i]*(DS.energy[i+1]-DS.energy[i])/average(DS.energy[i+1],DS.energy[i]))
+			DS.sumI += DS.resonseI[i]
+			// fmt.Println(sumI)
+		} else {
+			DS.resonseI = append(DS.resonseI, 0)
 		}
 	}
 }
@@ -69,4 +96,9 @@ func request(url *string) []byte {
 	body := get.Body
 	bytesBody, _ := ioutil.ReadAll(body)
 	return bytesBody
+}
+
+func average(lower, upper float64) float64 {
+	avr := (lower+upper)/2
+	return avr
 }
